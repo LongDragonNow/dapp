f24c2b224e8fd004f81d0b44e65babcaa03fcdea
LongDragonNow <163208684+LongDragonNow@users.noreply.github.com>
2024-03-13 10:11:57 +0300

    Add Speed Insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -30,8 +31,9 @@ export default function RootLayout({
       >
         <ContextProvider initialState={initialState}>
           {children}
-          <Analytics />
         </ContextProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
